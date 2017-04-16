5c0f9def3430003d83a87d877359611ae2c00a32
Alexandre Vazquez <avazquez@tibco.com>
2017-04-16 21:08:23 +0200

    no message

--- sonar-businessworks6-plugin/src/main/java/com/tibco/businessworks6/sonar/plugin/BwConstants.java
+++ sonar-businessworks6-plugin/src/main/java/com/tibco/businessworks6/sonar/plugin/BwConstants.java
@@ -3,20 +3,9 @@ package com.tibco.businessworks6.sonar.plugin;
 
 
 public interface BwConstants {
-
-      String BW_MIGRATION_PROFILE_NAME="MIGRATION-BW";
-      String PROP_RULE = "PROP_RULE";
-      String PROP_CONFIG = "PROP_CONFIG";
-      String PROP_SETTINGS = "PROP_SETTINGS";
       String REPOSITORY_KEY = "bw";
       String REPOSITORY_NAME = "bw";
       String BW_DEFAULT_PROFILE_NAME = "BW6";
-      String SONAR_PARAMETER_TYPE_TEXT="TEXT";
-      String SONAR_PARAMETER_TYPE_INTEGER="INTEGER";
-      String WIDTH_ALL_PERCENTAGE = "100%";
-      String MAX_HEIGHT_ERROR_PANEL = "600px";
-      String LINE_SENSOR_NOT_ALLOWED_DEFAULT = "jpg,png,gif,jpeg";
-      String LINE_SENSOR_NOT_ALLOWED="lineSensorNotAllowed";
       String LANGUAGE_KEY = "bw";  
       String LANGUAGE_NAME = "TIBCO ActiveMatrix BusinessWorks 6.x";
   
